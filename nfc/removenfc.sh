a030db4fec2379baf0aeb538be615af0037de41f
MrBaNkS <banksmi09@gmail.com>
2015-12-23 21:36:39 -0600

    update NFC remover script
    Change-Id: Ifbc073458e9f1200c93a3930925c59d44bd137a0

--- nfc/removenfc.sh
+++ nfc/removenfc.sh
@@ -3,7 +3,8 @@
 # Copyright (C) 2008 The Android Open-Source Project
 # Copyright (C) 2015 OmniROM Project
 #
-# by Victor Roque (victor.rooque@gmail.com)
+# originally by Victor Roque (victor.rooque@gmail.com)
+# cleaned up and edited for CM by BaNkS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +19,7 @@
 # limitations under the License.
 #
 
-RAW_ID=`cat /sys/devices/system/soc/soc0/raw_id`
+RAW_ID=$(cat /sys/devices/system/soc/soc0/raw_id)
 
 if [ $RAW_ID == 1974 ]; then
  rm -rf /system/app/NfcNci
