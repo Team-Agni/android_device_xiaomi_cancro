6306a9af30734b9f9c2ac689542ceee904a1dae9
MrBaNkS <banksmi09@gmail.com>
2015-12-23 22:41:43 -0600

    relocate libwcnss_qmi to wifi and actually build it
    Change-Id: I37299f352c3aed963f691df95960d0e5ed979120

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 # Copy additional target-specific files
 #----------------------------------------------------------------------
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
@@ -43,5 +44,14 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libwcnss_qmi
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES += hardware/qcom/wlan/wcnss_service
+LOCAL_SRC_FILES := wcnss_xiaomi_client.c
+LOCAL_CFLAGS += -Wall
+LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog libqminvapi
+include $(BUILD_SHARED_LIBRARY)
 
 endif
