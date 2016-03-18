1e50d23875a1c5477094a76b6c973003ae90a23d
MrBaNkS <banksmi09@gmail.com>
2016-03-18 06:23:28 -0500

    remove dexpreopt
    Change-Id: I7652f6f766022dcf74da5f0ca007903824cdd77a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,16 +169,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 # Keymaster
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
-# Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-DONT_DEXPREOPT_PREBUILTS := true
-
 # QCOM sepolicy
 include device/qcom/sepolicy/sepolicy.mk
 
